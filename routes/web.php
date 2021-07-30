87bf8a372b74a9fb583c545098016357a70c6e32
Mahmudul Hasan <hasanmahmuudl356@gmail.com>
2021-07-30 21:23:42 +0600

    Add Home index url

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,6 @@ use App\Http\Controllers\FrontendController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/image/preview', [FrontendController::class, 'imagePreview'])->name('image_view');
+Route::get('/', function (){
+    return view('welcome');
+});
